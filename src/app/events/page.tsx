cab805c135b9af14d775eed72eb7bd9aa61b56a8
karina <karinann514@gmail.com>
2026-02-09 18:41:25 -0500

    created events page

---
+++ src/app/events/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function Events() {
+    return (
+        <main>
+            <h1>
+                Events Page
+            </h1>
+        </main>
+    );
+}
